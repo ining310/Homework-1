cf249d275ef19ded026268221bc17dc77ebf50ee
ining310 <h94usu6@gmail.com>
2024-03-25 04:27:44 +0800

    Classroom 2 pass

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    bool private _isFirst = true;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if (_isFirst) {
+            _isFirst = false;
+            return 10001;
+        }
+        else {
+            return 123;
+        }
     }
 }
 
@@ -18,6 +26,14 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        IClassroomV2 classroomV2 = IClassroomV2(msg.sender);
+        bool _isFirst = !classroomV2.isEnrolled();
+        if (_isFirst) {
+            return 10001;
+        }
+        else {
+            return 123;
+        }
     }
 }
 
